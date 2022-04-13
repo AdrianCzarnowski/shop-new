a0cd3abed91efce21fd5bdc61010dd07a9278d65
aczarnowski <adriannnczarnowski@gmail.com>
2022-04-13 11:57:05 +0200

    with faker lib.

--- src/main/java/configuration/models/UserBuilder.java
+++ src/main/java/configuration/models/UserBuilder.java
@@ -1,9 +1,12 @@
 package configuration.models;
 
+import com.github.javafaker.Faker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UserBuilder {
 
     private static Logger log = LoggerFactory.getLogger("UserBuilder.class");
+
+    Faker faker = new Faker();
 }
