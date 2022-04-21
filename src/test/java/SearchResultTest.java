4b27c6edfa5a8d290c91befc914445f19406aab8
aczarnowski <adriannnczarnowski@gmail.com>
2022-04-21 14:17:22 +0200

    with SearchTest change

--- src/test/java/SearchResultTest.java
+++ src/test/java/SearchResultTest.java
@@ -9,28 +9,26 @@ import static org.hamcrest.Matchers.containsString;
 public class SearchResultTest extends TestBase {
 
     private static Logger log = LoggerFactory.getLogger("SearchResultTest.class");
+    private String product = homePage.randomProductNameHomePage();
 
-    @Test
-    public void resultOfSearchMatchToInputProductName() {
-        String product = homePage.randomProductNameHomePage();
+    private void fillSearchBox_RandomProductFromTheList(){
         log.info("<<<<<<Random product name: " + product);
-
-        searchResultPage
-                .fillSearchBox(product)
-                .clickSearchButton();
-        log.info("<<<<<<<<<<<<<Product name after after search: " + searchResultPage.getProductNameFromSearchField());
-
-        assertThat("Products don't match", searchResultPage.getProductNameFromSearchField(), containsString(product));
-    }
-    @Test
-    public void resulOfDropDownMatchToInputProductName(){
-        String product = homePage.randomProductNameHomePage();
-        log.info("<<<<<<Random product name: " + product);
-
         searchResultPage
                 .fillSearchBox(product);
+    }
+
+    @Test
+    public void searchResult_MatchTo_InputProductName() {
+        fillSearchBox_RandomProductFromTheList();
+        searchResultPage.clickSearchButton();
         log.info("<<<<<<<<<<<<<Product name after after search: " + searchResultPage.getProductNameFromSearchField());
-        assertThat("Products don't match", searchResultPage.getProductsNameFromDropDownList(), containsString(product));
+        assertThat(searchResultPage.getProductNameFromSearchField(), containsString(product));
+    }
+    @Test
+    public void dropDownResult_MatchTo_InputProductName(){
+        fillSearchBox_RandomProductFromTheList();
+        log.info("<<<<<<<<<<<<<Product name after after search: " + searchResultPage.getProductNameFromSearchField());
+        assertThat(searchResultPage.getProductsNameFromDropDownList(), containsString(product));
     }
 }
 
