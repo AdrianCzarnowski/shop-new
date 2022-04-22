3ac3bc04efd1bc457f719432fde1fc78ddba363c
aczarnowski <adriannnczarnowski@gmail.com>
2022-04-22 09:28:35 +0200

    with non static webDriver

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,8 +1,10 @@
 
 import helpers.DriverFactory;
-import org.junit.jupiter.api.AfterAll;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,27 +12,31 @@ import pages.SearchPage;
 import properties.App;
 import properties.reader.YamlReader;
 
-public class TestBase  {
 
-    private static Logger log = LoggerFactory.getLogger("TestBase.class");
-    protected static WebDriver driver;
+public class TestBase {
+    private static final Logger log = LoggerFactory.getLogger("TestBase.class");
+    protected WebDriver driver;
     private static App app;
+    public static SearchPage searchPage;
     private static DriverFactory driverFactory = new DriverFactory();
     private static YamlReader yamlReader = new YamlReader();
 
-    SearchPage searchPage = new SearchPage(driver);
-
-
 
     @BeforeAll
-    static  void beforeAll(){
+    static void beforeAll() {
         app = new App();
-        driver= driverFactory.getDriver
-                (yamlReader.getConfig().getBrowserConfig().getDriverEnum());
     }
-    @AfterAll
-    static void tearDown(){
+
+    @BeforeEach
+    void beforeEach() {
+        driver = driverFactory.getDriver(yamlReader.getConfig().getBrowserConfig().getDriverEnum());
+        searchPage = new SearchPage(driver);
+    }
+
+    @AfterEach
+    void tearDown() {
         driver.quit();
     }
 }
+
 
